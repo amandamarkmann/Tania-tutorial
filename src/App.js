b9f238aae99d5988697125fba804e6cbf095e914
amandamarkmann <amanda.markmann@live.se>
2020-01-29 11:57:13 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react'
+import Table from './Table'
+import Form from './Form'
+
+
+
+class App extends Component {
+
+  state = {
+    characters: [],
+    }
+  
+  removeCharacter = index => {
+    const { characters } = this.state
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      })
+    });
+  }
+
+handleSubmit = character => {
+  this.setState({ characters: [...this.state.characters, character]})
 }
 
-export default App;
+  render() {
+    const { characters } = this.state
+  
+    return (
+    <div className="container">
+    <Form handleSubmit={this.handleSubmit} />
+    <Table characterData={characters} removeCharacter = {this.removeCharacter} />
+  </div>
+)
+    }
+
+  
+  }
+  
+
+     
+
+
+export default App
